0a6a56a739f1b164882877e94439571d2975cf54
erineia <erineia@live.com>
2022-03-08 17:59:23 -0300

    primeiro commit

---
+++ ClienteTCP/ClienteTcp.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET,
+                          socket.SOCK_STREAM, 0)
+    except socket.error as e:
+        print('A conexão falhou!')
+        print('Erro: {}'.format(e))
+        sys.exit()
+
+    print('Socket criado com sucesso')
+
+    HostAlvo = input('Digite o host ou ip a ser conectado: ')
+    PortaAlvo= input('Digite a porta a ser conectada: ')
+
+
+    try:
+        s.connect((HostAlvo, int(PortaAlvo)))
+        print('Cliente TCP conectado com sucesso no Host: {} e na Porta: {}'.format(HostAlvo, PortaAlvo))
+        s.shutdown(2)
+    except socket.error as e:
+        print('Não foi possível conectar no Host {}!'. format(HostAlvo))
+        print('Erro: {}'.format(e))
+        sys.exit()
+
+
+if __name__ =='__main__':
+    main()
